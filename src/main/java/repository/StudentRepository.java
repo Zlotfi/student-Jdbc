d0cb97950ddc66d6b7924d6e3e6f3c7b2e1b2c84
victus <zlotfi758@gmail.com>
2024-12-24 12:07:52 +0330

    add save method

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -1,4 +1,29 @@
 package repository;
 
+import connection.JdbcConnection;
+import model.Student;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class StudentRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public StudentRepository() throws SQLException {
+    }
+
+    public int save(Student student) throws SQLException {
+        String add = "INSERT INTO student(firstName, lastName, studentNumber, userName, password) VALUES(?,?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, student.getFirstName());
+        preparedStatement.setString(2, student.getLastName());
+        preparedStatement.setString(3, student.getStudentNumber());
+        preparedStatement.setString(4, student.getUserName());
+        preparedStatement.setString(5, student.getPassword());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
