250e507078e69908ceca621e79c3a43893ca9b8f
victus <zlotfi758@gmail.com>
2024-12-24 16:25:25 +0330

    add main method for test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,29 @@
+import menu.Menu;
+import model.Courses;
+import model.Student;
+import repository.CourseRepository;
+import repository.StudentRepository;
+import service.StudentService;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+//        StudentRepository studentRepository = new StudentRepository();
+//        Student student = new Student
+//                (null, "zahra", "lotfi", "123456789","zlotfi80","1234");
+//        studentRepository.save(student);
+//        CourseRepository courseRepository = new CourseRepository();
+//        Courses courses = new Courses(null,"riazi",1);
+//        courseRepository.save(courses);
+        StudentService studentService = new StudentService();
+//        studentService.register();
+//        studentService.login();
+//        studentService.changeFirstName();
+
+//        studentService.delete();
+
+        Menu menu = new Menu();
+        menu.publicMenu();
+    }
 }
