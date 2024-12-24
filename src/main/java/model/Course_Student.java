f8c3b8675f8c58e269fe3aa4c0b1b8d81980ff61
victus <zlotfi758@gmail.com>
2024-12-24 12:28:13 +0330

    add attribute and constructor, getter and setter

--- src/main/java/model/Course_Student.java
+++ src/main/java/model/Course_Student.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Course_Student {
+    private Integer course_id;
+    private Integer student_id;
+
+    public Course_Student(Integer course_id, Integer student_id) {
+        this.course_id = course_id;
+        this.student_id = student_id;
+    }
+
+    public Integer getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(Integer course_id) {
+        this.course_id = course_id;
+    }
+
+    public Integer getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(Integer student_id) {
+        this.student_id = student_id;
+    }
 }
