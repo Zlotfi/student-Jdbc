926206e408ee1046c08c979d87d19e289c7382f0
victus <zlotfi758@gmail.com>
2024-12-24 11:22:05 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,4 +1,67 @@
 package model;
 
 public class Student {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String studentNumber;
+    private String userName;
+    private String password;
+
+    public Student(Integer id, String firstName, String lastName, String studentNumber, String userName, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
