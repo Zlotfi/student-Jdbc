aca088a425d312a3d394354141bcccbbca77bc76
victus <zlotfi758@gmail.com>
2024-12-25 10:52:17 +0330

    add some connection to classes

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,4 +1,24 @@
 package utility;
 
+import connection.JdbcConnection;
+import repository.StudentRepository;
+import service.StudentService;
+
+import java.sql.Connection;
+
 public class ApplicationContext {
+
+    private static final Connection CONNECTION;
+    private static final StudentRepository STUDENT_REPOSITORY;
+    private static final StudentService STUDENT_SERVICE;
+
+    static {
+        CONNECTION = JdbcConnection.getConnection();
+        STUDENT_REPOSITORY = new StudentRepository(CONNECTION);
+        STUDENT_SERVICE = new StudentService(STUDENT_REPOSITORY);
+    }
+
+    public static StudentService getStudentService(){
+        return STUDENT_SERVICE;
+    }
 }
